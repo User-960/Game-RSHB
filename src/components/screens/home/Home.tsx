b1fb3267818d18e08eb82de3cef723cd589e13bf
Ivan <ivanvolodin960@gmail.com>
2023-07-25 00:45:30 +0300

    feat: create component InfoBox

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import ButtonIcon from '@/components/ui/button/buttonIcon/ButtonIcon'
 import { GlobalSvgSelector } from '@/components/ui/global-svg-selector/GlobalSvgSelector'
+import InfoBox from '@/components/ui/info-box/InfoBox'
 
 import Layout from '@/components/layout/Layout'
 import { IMeta } from '@/components/seo/meta.interface'
+
+const text = `Дорогой друг, приветствую тебя в 2025 году! Здесь сельское хозяйство играет важную роль в обеспечении пищей населения городов и деревень. У нас беда -  урожая на всех не хватает! Помоги фермерам оптимизировать выращивание и сбор растительных культур. Используй современные технологии и автоматизированные системы.`
 
 const Home: FC = () => {
 	const meta: IMeta = {
@@ -12,11 +15,21 @@ const Home: FC = () => {
 		description: 'RSHB Game'
 	}
 
+	const [infoShow, setInfoShow] = useState<boolean>(true)
+
 	return (
 		<Layout meta={meta}>
 			<ButtonIcon name='Меню' size='medium'>
 				<GlobalSvgSelector id='menu' />
 			</ButtonIcon>
+
+			{infoShow && (
+				<InfoBox
+					text={text}
+					size='large'
+					onClick={() => setInfoShow(prev => !prev)}
+				/>
+			)}
 		</Layout>
 	)
 }
---
+++ src/components/ui/info-box/InfoBox.tsx
@@ -0,0 +1,24 @@
+import cn from 'clsx'
+import { Dispatch, FC, SetStateAction } from 'react'
+
+import styles from './InfoBox.module.scss'
+
+interface IInfoBoxProps {
+	text: string
+	size: string
+	//clickHandler: any
+	onClick: (value: SetStateAction<boolean>) => void
+}
+
+const InfoBox: FC<IInfoBoxProps> = ({ text, size, onClick }) => {
+	return (
+		<div className={cn(styles.wrapper, styles[size])}>
+			<p>{text}</p>
+			<button className={styles.button} onClick={() => onClick(true)}>
+				Далее
+			</button>
+		</div>
+	)
+}
+
+export default InfoBox
